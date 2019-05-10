7675a728fce71a45f07a3eed55d5a788115d743c
Jarnin Fang <jfang@digium.com>
2019-05-10 15:59:21 -0700

    Added stuff

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import 'semantic-ui-css/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>, 
     document.getElementById('root'));
 
